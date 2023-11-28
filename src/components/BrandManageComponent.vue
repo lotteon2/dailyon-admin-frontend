7b8a032e33981a8c3581a7fdd2e7ed691df55ea9
wakkpu <ho0524870@gmail.com>
2023-11-28 21:30:44 +0900

    [FEAT] 테이블 css

--- src/components/BrandManageComponent.vue
+++ src/components/BrandManageComponent.vue
@@ -1,11 +1,36 @@
 <script setup lang="ts">
-
+const exampleBrands = [
+  { brandId: 1, name: "brand1" },
+  { brandId: 2, name: "brand2" },
+  { brandId: 3, name: "brand3" }
+]
 </script>
 
 <template>
-<h1>Brand</h1>
+  <div class="container">
+    <div class="button-block">
+      <button>브랜드 등록</button>
+    </div>
+    <div class="table-block">
+      <table>
+        <thead>
+          <tr>
+            <th>브랜드 id</th>
+            <th>브랜드 이름</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="brand in exampleBrands" :key="brand.brandId">
+            <td>{{ brand.brandId }}</td>
+            <td>{{ brand.name }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+@import url("@/assets/css/brand-manage.css");
 </style>
--- src/components/SideBarComponent.vue
+++ src/components/SideBarComponent.vue
@@ -4,14 +4,10 @@
   <div class="sidebar">
     <div class="sidebar-elements">
       <RouterLink to="/dashboard" class="sidebar-element">
-        <div><img src="@/assets/images/Dashboard_White.png" alt="dashboard" /></div>
-        <div>대시보드</div>
+        <img src="@/assets/images/Dashboard_White.png" alt="dashboard" />대시보드
       </RouterLink>
       <RouterLink to="/manage-product" class="sidebar-element">
         <img src="@/assets/images/Product_White.png" alt="product" />상품 관리
-      </RouterLink>
-      <RouterLink to="/manage-brand" class="sidebar-element">
-        <img src="@/assets/images/Category_White.png" alt="brand" />브랜드 관리
       </RouterLink>
       <RouterLink to="/manage-banner" class="sidebar-element">
         <img src="@/assets/images/Banner_White.png" alt="banner" />배너 관리
@@ -21,6 +17,9 @@
       </RouterLink>
       <RouterLink to="/manage-raffle" class="sidebar-element">
         <img src="@/assets/images/Raffle_White.png" alt="raffle" />응모 관리
+      </RouterLink>
+      <RouterLink to="/manage-brand" class="sidebar-element">
+        <img src="@/assets/images/Category_White.png" alt="brand" />브랜드 관리
       </RouterLink>
       <RouterLink to="/manage-category" class="sidebar-element">
         <img src="@/assets/images/Category_White.png" alt="category" />카테고리 관리
