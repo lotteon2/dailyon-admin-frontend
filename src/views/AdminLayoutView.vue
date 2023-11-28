310152c9f3fb71f8062d0874bfcfcba11832b8a5
wakkpu <ho0524870@gmail.com>
2023-11-28 17:53:24 +0900

    [INIT] 관리자 프론트 세팅 - 헤더 - 관리자 레이아웃

---
+++ src/views/AdminLayoutView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <div class="sidebar">
+      <div class="sidebar-elements">
+        <div class="sidebar-element">
+          <img src="@/assets/images/Dashboard_White.png" alt="dashboard" />대시보드
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Product_White.png" alt="product" />상품 관리
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Category_White.png" alt="brand" />브랜드 관리
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Banner_White.png" alt="banner" />배너 관리
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Coupon_White.png" alt="coupon" />쿠폰 관리
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Raffle_White.png" alt="raffle" />응모 관리
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Category_White.png" alt="category" />카테고리 관리
+        </div>
+        <div class="sidebar-element">
+          <img src="@/assets/images/Category_White.png" alt="product-size" />치수 관리
+        </div>
+      </div>
+    </div>
+    <h1>COMPONENTS</h1>
+  </div>
+</template>
+
+<style scoped>
+@import "@/assets/css/admin-layout.css";
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>로그인 페이지</template>
+
+<style scoped></style>
