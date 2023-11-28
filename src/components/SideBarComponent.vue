f476d53a5febf6406253f038aedec6eb4202a8f7
wakkpu <ho0524870@gmail.com>
2023-11-28 20:34:34 +0900

    [FIX] 사이드바 컴포넌트 분리

---
+++ src/components/SideBarComponent.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="sidebar">
+    <div class="sidebar-elements">
+      <RouterLink to="/dashboard" class="sidebar-element">
+        <div><img src="@/assets/images/Dashboard_White.png" alt="dashboard" /></div>
+        <div>대시보드</div>
+      </RouterLink>
+      <RouterLink to="/manage-product" class="sidebar-element">
+        <img src="@/assets/images/Product_White.png" alt="product" />상품 관리
+      </RouterLink>
+      <RouterLink to="/manage-brand" class="sidebar-element">
+        <img src="@/assets/images/Category_White.png" alt="brand" />브랜드 관리
+      </RouterLink>
+      <RouterLink to="/manage-banner" class="sidebar-element">
+        <img src="@/assets/images/Banner_White.png" alt="banner" />배너 관리
+      </RouterLink>
+      <RouterLink to="/manage-coupon" class="sidebar-element">
+        <img src="@/assets/images/Coupon_White.png" alt="coupon" />쿠폰 관리
+      </RouterLink>
+      <RouterLink to="/manage-raffle" class="sidebar-element">
+        <img src="@/assets/images/Raffle_White.png" alt="raffle" />응모 관리
+      </RouterLink>
+      <RouterLink to="/manage-category" class="sidebar-element">
+        <img src="@/assets/images/Category_White.png" alt="category" />카테고리 관리
+      </RouterLink>
+      <RouterLink to="/manage-product-size" class="sidebar-element">
+        <img src="@/assets/images/Category_White.png" alt="product-size" />치수 관리
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "@/assets/css/side-bar.css";
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AdminLayoutView from '@/views/AdminLayoutView.vue'
-import LoginView from '@/views/LoginView.vue'
+import { createRouter, createWebHistory } from "vue-router"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: "/",
+      name: "login",
+      component: () => import("@/views/LoginView.vue")
     },
     {
-      path: '/',
-      name: 'home',
-      component: AdminLayoutView
+      path: "/main",
+      name: "main",
+      component: () => import("@/views/AdminLayoutView.vue"),
+      children: [
+        {
+          path: "/manage-brand",
+          name: "manage-brand",
+          component: () => import("@/components/BrandManageComponent.vue")
+        },
+        {
+          path: "/manage-category",
+          name: "manage-category",
+          component: () => import("@/components/CategoryManageComponent.vue")
+        },
+        {
+          path: "/manage-coupon",
+          name: "manage-coupon",
+          component: () => import("@/components/CouponManagerComponent.vue")
+        },
+        {
+          path: "/manage-banner",
+          name: "manage-banner",
+          component: () => import("@/components/BannerManageComponent.vue")
+        },
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          component: () => import("@/components/DashBoardComponent.vue")
+        },
+        {
+          path: "/manage-product",
+          name: "manage-product",
+          component: () => import("@/components/ProductManageComponent.vue")
+        },
+        {
+          path: "/manage-product-size",
+          name: "manage-product-size",
+          component: () => import("@/components/ProductSizeManageComponent.vue")
+        },
+        {
+          path: "/manage-raffle",
+          name: "manage-raffle",
+          component: () => import("@/components/RaffleManageComponent.vue")
+        }
+      ]
     }
   ]
 })
