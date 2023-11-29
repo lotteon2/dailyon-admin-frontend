5507512a4adf2634c684d812cec02ce8eb1dfa60
wakkpu <ho0524870@gmail.com>
2023-11-29 15:12:26 +0900

    O2F-462 [FEAT] 브랜드 목록 컴포넌트

--- src/components/BrandManageComponent.vue
+++ src/components/BrandManageComponent.vue
@@ -1,15 +1,72 @@
 <script setup lang="ts">
-const exampleBrands = [
+import { ref } from "vue"
+import PaginationComponent from "@/components/PaginationComponent.vue"
+import BrandUpdateModal from "@/components/BrandUpdateModal.vue"
+import BrandCreateModal from "@/components/BrandCreateModal.vue"
+
+let exampleBrands = [
   { brandId: 1, name: "brand1" },
   { brandId: 2, name: "brand2" },
-  { brandId: 3, name: "brand3" }
+  { brandId: 3, name: "brand3" },
+  { brandId: 4, name: "brand4" },
+  { brandId: 5, name: "brand5" }
 ]
+
+const isCreateModalVisible = ref(false)
+const isUpdateModalVisible = ref(false)
+const selectedBrandId = ref()
+const selectedBrandName = ref()
+
+const openUpdateModal = (brandId: Number, brandName: String) => {
+  isUpdateModalVisible.value = true
+  selectedBrandId.value = brandId
+  selectedBrandName.value = brandName
+}
+
+const openCreateModal = () => {
+  isCreateModalVisible.value = true
+}
+
+const closeUpdateModal = () => {
+  isUpdateModalVisible.value = false
+}
+
+const closeCreateModal = () => {
+  isCreateModalVisible.value = false
+}
+
+const createBrand = (data: any) => {
+  alert(`created id: ${data.id}, name: ${data.name}`)
+}
+
+const updateBrand = (data: any) => {
+  alert(`updated id: ${data.id}, name: ${data.name}`)
+}
+
+const deleteButton = (brandId: Number) => {
+  /* TODO
+   axios.delete해서 200이라면 목록에서 해당 id 삭제.
+   */
+  alert(`deleted ${brandId}`)
+}
 </script>
 
 <template>
   <div class="container">
+    <BrandUpdateModal
+      :brand-id="selectedBrandId"
+      :brand-name="selectedBrandName"
+      :show-modal="isUpdateModalVisible"
+      @update-brand="updateBrand"
+      @close-update-modal="closeUpdateModal"
+    />
+    <BrandCreateModal
+      :show-modal="isCreateModalVisible"
+      @create-brand="createBrand"
+      @close-create-modal="closeCreateModal"
+    />
     <div class="button-block">
-      <button class="createBtn">브랜드 등록</button>
+      <button class="createBtn" @click="openCreateModal">브랜드 등록</button>
     </div>
     <div class="table-block">
       <table>
@@ -25,13 +82,16 @@ const exampleBrands = [
             <td>{{ brand.brandId }}</td>
             <td>{{ brand.name }}</td>
             <td>
-                <button class="updateBtn">수정</button>
-                <button class="deleteBtn">삭제</button>
+              <button class="updateBtn" @click="openUpdateModal(brand.brandId, brand.name)">
+                수정
+              </button>
+              <button class="deleteBtn" @click="deleteButton(brand.brandId)">삭제</button>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
+    <PaginationComponent />
   </div>
 </template>
 
