e96386fedb7b53d01acb5dbd668a52fdb5d64e9f
wakkpu <ho0524870@gmail.com>
2023-12-04 16:05:17 +0900

    [FEAT] added dto

--- src/apis/category/dto/CategoryResponse.ts
+++ src/apis/category/dto/CategoryResponse.ts
@@ -1,6 +1,16 @@
+export interface Category {
+  id: number | null
+  name: string | null
+}
+
 export interface ReadCategoryResponse {
   id: number
   name: string
   masterCategoryId: null | number
   masterCategoryName: null | string
+}
+
+export interface UpdateCategoryResponse {
+  index: number
+  name: string
 }
