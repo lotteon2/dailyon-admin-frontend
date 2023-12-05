654e140a36803826b5e9dfaf3cec5c979fa37c05
CessnaJ <eon8748@gmail.com>
2023-12-06 01:12:19 +0900

    [FEAT] 관리자 쿠폰조회/생성 API 작성

---
+++ src/apis/coupon/CouponClient.ts
@@ -0,0 +1,48 @@
+import { authAxiosInstance } from "@/apis/utils"
+import type { AxiosResponse } from "axios"
+import type { CouponCreateRequest, CouponInfo, CouponInfoPageResponse
+} from "@/apis/coupon/CouponDto"
+
+const COUPON_ADMIN_PREFIX: string = "" // TODO: /admin 추가
+const COUPON_PREFIX: string = "/coupons"
+
+export const createCouponInfo = async (body: CouponCreateRequest): Promise<AxiosResponse> => {
+  return await authAxiosInstance.post(COUPON_ADMIN_PREFIX + COUPON_PREFIX, body)
+}
+
+export const getCouponInfoPage = async (size?: number, page?: number): Promise<CouponPageResponse> => {
+  let queryParams: string[] = [];
+  if (size != null) {
+    queryParams.push(`size=${size}`);
+  }
+  if (page != null) {
+    queryParams.push(`page=${page}`);
+  }
+
+  let url = COUPON_ADMIN_PREFIX + COUPON_PREFIX;
+  if (queryParams.length > 0) {
+    url += `?${queryParams.join('&')}`;
+  }
+  try {
+    const response = await authAxiosInstance(url);
+    return response.data as CouponInfoPageResponse; // typecast
+  } catch (err) {
+    console.error("Error fetching couponInfo:", err);
+    throw err;
+  }
+};
+
+
+// export const getProductSizesByCategory = async (categoryId: number): Promise<AxiosResponse> => {
+//   return await authAxiosInstance.get(COUPON_ADMIN_PREFIX + COUPON_SIZE_PREFIX + "/" + categoryId)
+// }
+
+
+// export const updateProductSize = async (
+//   body: CouponCreateRequest
+// ): Promise<AxiosResponse> => {
+//   return await authAxiosInstance.put(
+//     COUPON_ADMIN_PREFIX + COUPON_SIZE_PREFIX,
+//     body
+//   )
+// }
