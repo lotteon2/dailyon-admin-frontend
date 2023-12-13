889ff7b2a867d7dfc39618e2da213d0c28d7225e
CessnaJ <eon8748@gmail.com>
2023-12-13 11:39:26 +0900

    [FEAT/FIX] 쿠폰 이름 자동완성 기능 작성, 쿠폰 생성 모달 타입에러 해결

--- src/components/coupon/CouponCreateModal.vue
+++ src/components/coupon/CouponCreateModal.vue
@@ -9,7 +9,7 @@
           <!-- Coupon Name -->
           <div class="form-group">
             <label for="couponName">쿠폰 이름</label>
-            <input type="text" id="couponName" placeholder="적용대상, 할인 값 작성시 자동완성됩니다." v-model="coupon.name" required />
+            <input type="text" id="couponName" placeholder="적용대상, 할인 값 작성시 자동완성됩니다." v-model="couponCreateRequest.name" required />
           </div>
 
           <!-- Applies To (Select Product or Category Leaf Node) -->
@@ -22,15 +22,15 @@
               <label for="targetTypeCategory">카테고리</label>
             </div>
             <div v-if="targetType === 'PRODUCT'">
-              <input type="text" id="productSearch" v-model="searchQuery" @input="searchProducts" placeholder="상품 검색" autocomplete="off" />
+              <input type="text" id="productSearch" v-model="searchQuery" @input="performSearchProducts" placeholder="상품 검색" autocomplete="off" />
               <ul v-if="searchResults.length" class="search-results">
-                <li v-for="result in searchResults" :key="result.id" @click="selectAppliesTo(result, 'PRODUCT')">
-                  {{ result.name }} ({{ result.brandName }})
+                <li v-for="result in searchResults" :key="result.id" @click="selectProduct(result)">
+                  {{ result.name }}
                 </li>
               </ul>
             </div>
             <div v-else-if="targetType === 'CATEGORY'">
-              <select id="categorySelect" v-model="selectedCategoryId">
+              <select id="categorySelect" @change="handleCategoryChange($event)">
                 <option v-for="category in categories" :key="category.id" :value="category.id">
                   {{ category.name }}
                 </option>
@@ -41,14 +41,14 @@
           <!-- Discount Type and Value -->
           <div class="form-group">
             <label for="discountType">할인 타입</label>
-            <select id="discountType" v-model="coupon.discountType" required>
+            <select id="discountType" v-model="couponCreateRequest.discountType" required>
               <option value="PERCENTAGE">정률 할인</option>
               <option value="FIXED_AMOUNT">정액 할인</option>
             </select>
           </div>
           <div class="form-group">
             <label for="discountValue">할인 값</label>
-            <input type="number" id="discountValue" v-model="coupon.discountValue" min="0" required />
+            <input type="number" id="discountValue" v-model="couponCreateRequest.discountValue" min="0" required />
             <!-- <span v-if="coupon.discountType === 'PERCENTAGE'">%</span> -->
             <!-- <span v-else>원</span> -->
           </div>
@@ -56,17 +56,17 @@
           <!-- Start and End Dates -->
           <div class="form-group">
             <label for="startAt">이벤트 시작일시</label>
-            <input type="datetime-local" id="startAt" v-model="coupon.startAt" required />
+            <input type="datetime-local" id="startAt" v-model="couponCreateRequest.startAt" required />
           </div>
           <div class="form-group">
             <label for="endAt">이벤트 종료일시</label>
-            <input type="datetime-local" id="endAt" v-model="coupon.endAt" required />
+            <input type="datetime-local" id="endAt" v-model="couponCreateRequest.endAt" required />
           </div>
   
           <!-- Issued Quantity -->
           <div class="form-group">
             <label for="issuedQuantity">발행 개수</label>
-            <input type="number" id="issuedQuantity" v-model="coupon.issuedQuantity" min="0" required />
+            <input type="number" id="issuedQuantity" v-model="couponCreateRequest.issuedQuantity" min="0" required />
             <!-- <span>개</span> -->
           </div>
 
@@ -74,7 +74,7 @@
           <div class="form-group">
             <label for="requiresConcurrencyControl">동시성 관리 여부(특가 여부)</label>
             
-            <select id="requiresConcurrencyControl" v-model="coupon.requiresConcurrencyControl" required>
+            <select id="requiresConcurrencyControl" v-model="couponCreateRequest.requiresConcurrencyControl" required>
               <option value="false">아니오</option>
               <option value="true">예</option>
             </select>
@@ -84,12 +84,12 @@
           <!-- Optional Fields -->
           <div class="form-group">
             <label for="minPurchaseAmount">최소 구매금액</label>
-            <input type="number" id="minPurchaseAmount" placeholder="0" v-model="coupon.minPurchaseAmount" min="0" />
+            <input type="number" id="minPurchaseAmount" placeholder="0" v-model="couponCreateRequest.minPurchaseAmount" min="0" />
             <!-- <span>원</span> -->
           </div>
           <div class="form-group">
             <label for="maxDiscountAmount">최대 할인금액</label>
-            <input type="number" id="maxDiscountAmount" placeholder="필수값이 아닙니다." v-model="coupon.maxDiscountAmount" min="0" />
+            <input type="number" id="maxDiscountAmount" placeholder="필수값이 아닙니다." v-model="couponCreateRequest.maxDiscountAmount" min="0" />
             <!-- <span>원</span> -->
           </div>
   
@@ -104,9 +104,9 @@
 </template>
   
 <script setup lang="ts">
-import { ref, onMounted, defineEmits, defineProps } from "vue";
-import { createCouponInfo } from "@/apis/coupon/CouponClient";
-import type { CouponCreateRequest } from "@/apis/coupon/CouponDto";
+import { ref, onMounted, defineEmits, defineProps, computed, watch } from "vue";
+import { createCouponInfo, searchProducts, fetchCategories } from "@/apis/coupon/CouponClient";
+import type { CouponCreateRequest, ReadProductResponse, ReadChildrenCategoryResponse } from "@/apis/coupon/CouponDto";
 import axios from "axios"; // assuming you are using axios for HTTP requests
 
 const emits = defineEmits(["close-create-modal"]);
@@ -114,22 +114,30 @@ const props = defineProps({
   showModal: Boolean
 });
 
-const coupon = ref<CouponCreateRequest>({
+const couponCreateRequest = ref<CouponCreateRequest>({
   name: "",
   discountType: "PERCENTAGE",
   discountValue: 0,
+
   startAt: "",
   endAt: "",
+
   issuedQuantity: 0,
+
   appliesToType: "",
   appliesToId: 0,
   appliesToName: "",
+
   requiresConcurrencyControl: false,
+  targetImgUrl: "",
+
+  minPurchaseAmount: 0,
+  maxDiscountAmount: undefined
 });
 
 const searchQuery = ref("");
-const searchResults = ref([]);
-const categories = ref([]);
+const searchResults = ref<ReadProductResponse[]>([]);
+const categories = ref<ReadChildrenCategoryResponse[]>([]);
 const targetType = ref("PRODUCT"); // Default to product
 const selectedCategoryId = ref(null);
 
@@ -137,44 +145,28 @@ const closeModal = () => {
   emits("close-create-modal");
 };
 
-const fetchCategories = async () => {
+const loadCategories = async () => {
   try {
-    const response = await axios.get("/admin/categories/leaf");
-    categories.value = response.data.categoryResponses;
+    const categoryData = await fetchCategories();
+    categories.value = categoryData;
   } catch (error) {
-    // Handle error fetching categories
-    console.error("Failed to fetch categories", error);
+    console.error("Failed to loadCategories", error);
   }
 };
 
-const searchProducts = async () => {
+const performSearchProducts = async () => {
   try {
-    if (searchQuery.value.trim() !== "") {
-      const response = await axios.get(`/product/search`, {
-        params: { lastId: 0, query: searchQuery.value },
-      });
-      searchResults.value = response.data.productResponses;
-    } else {
-      searchResults.value = [];
-    }
+    const productData = await searchProducts(searchQuery.value);
+    searchResults.value = productData;
   } catch (error) {
-    // Handle error searching products
-    console.error("Failed to search products", error);
+    console.error("Failed to performSearchProducts", error);
   }
 };
 
 const createCoupon = async () => {
-  if (targetType.value === "CATEGORY" && selectedCategoryId.value) {
-    const selectedCategory = categories.value.find(c => c.id === selectedCategoryId.value);
-    coupon.value.appliesToId = selectedCategory.id;
-    coupon.value.appliesToName = selectedCategory.name;
-    coupon.value.appliesToType = targetType.value;
-  }
-
   try {
-    const response = await createCouponInfo(coupon.value);
+    const response = await createCouponInfo(couponCreateRequest.value);
     if (response.status === 201) {  // Ensure you have got the 201 status indicating a successful creation
-      emits("coupon-created");  // Emit an event to the parent component
       closeModal();
     }
     // Additional state updates
@@ -183,14 +175,67 @@ const createCoupon = async () => {
   }
 };
 
-const selectAppliesTo = (result, type) => {
-  coupon.value.appliesToId = result.id;
-  coupon.value.appliesToName = result.name;
-  coupon.value.appliesToType = type;
+const selectProduct = (result: ReadProductResponse) => {
+  couponCreateRequest.value.appliesToType = 'PRODUCT';
+  couponCreateRequest.value.appliesToId = result.id;
+  couponCreateRequest.value.appliesToName = result.name;
+  couponCreateRequest.value.targetImgUrl = result.imgUrl;
   searchResults.value = [];
+  couponCreateRequest.value.name = couponName.value;
 };
 
-onMounted(fetchCategories);
+const handleCategoryChange = (event: Event) => {
+  const selectElement = event.target as HTMLSelectElement;
+  const selectedIndex = selectElement.selectedIndex;
+  const selectedOption = selectElement.options[selectedIndex];
+  const selectedCategoryId = selectedOption.value;
+  
+  const selectedCategory = categories.value.find(category => `${category.id}` === selectedCategoryId);
+
+  if (selectedCategory) {
+    selectCategory(selectedCategory);
+  }
+};
+
+const selectCategory = (category: ReadChildrenCategoryResponse) => {
+  couponCreateRequest.value.appliesToType = "CATEGORY";
+  couponCreateRequest.value.appliesToId = category.id;
+  couponCreateRequest.value.appliesToName = category.name;
+  couponCreateRequest.value.targetImgUrl = ""; // Categories do not have images
+  couponCreateRequest.value.name = couponName.value;
+};
+
+const couponName = computed(() => {
+  let nameParts: string[] = [];
+
+  if (couponCreateRequest.value.requiresConcurrencyControl) {
+    nameParts.push("[특가 할인]");
+  }
+  
+  if (couponCreateRequest.value.appliesToName) {
+    nameParts.push(couponCreateRequest.value.appliesToType === "CATEGORY" 
+                ? `${couponCreateRequest.value.appliesToName} 카테고리` 
+                : couponCreateRequest.value.appliesToName);
+  }
+
+  if (couponCreateRequest.value.discountType === "PERCENTAGE") {
+    nameParts.push(`${couponCreateRequest.value.discountValue}% 할인`);
+  } else if (couponCreateRequest.value.discountType === "FIXED_AMOUNT") {
+    nameParts.push(`${new Intl.NumberFormat().format(couponCreateRequest.value.discountValue)}원 할인`);
+  }
+
+  if (nameParts.length > 0) {
+    nameParts.push("쿠폰");
+  }
+
+  return nameParts.join(" ");
+});
+
+watch(couponName, (newName) => {
+  couponCreateRequest.value.name = newName;
+});
+
+onMounted(loadCategories);
 </script>
 
 <style scoped>
