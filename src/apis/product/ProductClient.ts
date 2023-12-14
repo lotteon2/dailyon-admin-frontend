2ecadeb77e0796f75755fca147da155ecae88e67
wakkpu <ho0524870@gmail.com>
2023-12-14 21:40:27 +0900

    O2F-819 [FEAT] 관리자 상품 수정

--- src/apis/product/ProductClient.ts
+++ src/apis/product/ProductClient.ts
@@ -1,7 +1,11 @@
 import { authAxiosInstance } from "@/apis/utils"
 import type { AxiosResponse } from "axios"
 
-import type { CreateProductRequest, ReadProductPageRequest } from "@/apis/product/ProductDto"
+import type {
+  CreateProductRequest,
+  ReadProductPageRequest,
+  UpdateProductRequest
+} from "@/apis/product/ProductDto"
 
 const PRODUCT_SERVICE_PREFIX: string = "/product-service"
 const PRODUCT_ADMIN_PREFIX: string = "/admin"
@@ -18,6 +22,16 @@ export const getProductPages = async (request: ReadProductPageRequest): Promise<
 export const createProduct = async (request: CreateProductRequest): Promise<AxiosResponse> => {
   return await authAxiosInstance.post(
     `${PRODUCT_SERVICE_PREFIX}${PRODUCT_ADMIN_PREFIX}${PRODUCT_PREFIX}`,
+    request
+  )
+}
+
+export const updateProduct = async (
+  productId: number,
+  request: UpdateProductRequest
+): Promise<AxiosResponse> => {
+  return await authAxiosInstance.put(
+    `${PRODUCT_SERVICE_PREFIX}${PRODUCT_ADMIN_PREFIX}${PRODUCT_PREFIX}/${productId}`,
     request
   )
 }
