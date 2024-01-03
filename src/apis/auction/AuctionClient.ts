20c4e6c65a4048e5b086954473f25dd48cac65d0
wakkpu <ho0524870@gmail.com>
2024-01-03 21:24:09 +0900

    O2F-1070 [FEAT] 경매 목록 조회 script 영역

---
+++ src/apis/auction/AuctionClient.ts
@@ -0,0 +1,46 @@
+import { authAxiosInstance } from "@/apis/utils"
+import { AxiosError, type AxiosResponse } from "axios"
+import type {
+  CreateAuctionRequest,
+  CreateAuctionResponse,
+  ReadAuctionPageResponse
+} from "@/apis/auction/AuctionDto"
+
+const AUCTION_SERVICE_PREFIX: string = "/auction-service"
+const AUCTION_ADMIN_PREFIX: string = "/admin"
+const AUCTION_PREFIX: string = "/auction"
+
+export const createAuction = async (request: CreateAuctionRequest): Promise<AxiosResponse> => {
+  return await authAxiosInstance.post(
+    `${AUCTION_SERVICE_PREFIX}${AUCTION_ADMIN_PREFIX}${AUCTION_PREFIX}`,
+    request
+  )
+}
+
+export const readAuctions = async (
+  page: number,
+  size: number
+): Promise<ReadAuctionPageResponse> => {
+  try {
+    const { data }: ReadAuctionPageResponse = authAxiosInstance.get(
+      `${AUCTION_SERVICE_PREFIX}${AUCTION_ADMIN_PREFIX}${AUCTION_PREFIX}`,
+      {
+        params: { page: page, size: size }
+      }
+    )
+    return data
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        if (error.response.status >= 400) {
+          alert(error.response.data.message)
+          console.error(`Client Error=${error.response.data.message}`)
+        } else if (error.response.status < 500) {
+          alert("서버 내부 오류")
+          console.error("Internal Server Error")
+        }
+      }
+    }
+    throw error
+  }
+}
