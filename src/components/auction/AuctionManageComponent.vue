f80913ba16580bd9d21e9014db85e488e3f86b53
wakkpu <ho0524870@gmail.com>
2024-01-03 21:23:54 +0900

    O2F-1069 [FEAT] 경매 목록 조회 template 영역

--- src/components/auction/AuctionManageComponent.vue
+++ src/components/auction/AuctionManageComponent.vue
@@ -1,7 +1,91 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PaginationComponent from "@/components/PaginationComponent.vue"
+import { onBeforeMount, ref, watch } from "vue"
+import { readAuctions } from "@/apis/auction/AuctionClient"
+import type { ReadAuctionPageResponse, ReadAuctionResponse } from "@/apis/auction/AuctionDto"
+
+const requestSize: number = 5
+const requestPage = ref<number>(0)
+const totalPages = ref<number>(0)
+const totalElements = ref<number>(0)
+const auctions = ref<ReadAuctionResponse[]>([])
+const isCreateModalVisible = ref<boolean>(false)
+
+const openCreateModal = () => {
+  isCreateModalVisible.value = true
+  alert("경매 등록")
+}
+
+const formatDate = (localDateTime: string) => {
+  // Assuming localDateTime is in ISO string format, adjust as necessary
+  return localDateTime.replace(
+    /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}).*$/,
+    "$1년 $2월 $3일 $4:$5"
+  )
+}
+
+const onChangePage = async (page: number) => {
+  if (0 <= page && page < totalPages.value) {
+    requestPage.value = page
+  }
+}
+
+const initData = () => {
+  const response: ReadAuctionPageResponse = readAuctions(requestPage.value, requestSize)
+
+  totalElements.value = response.totalElements
+  totalPages.value = response.totalPages
+  auctions.value = response.responses
+}
+
+watch(requestPage, async (afterPage: number, beforePage: number) => {
+  if (afterPage < totalPages.value) {
+    const response: ReadAuctionPageResponse = readAuctions(afterPage, requestSize)
+
+    totalElements.value = response.totalElements
+    totalPages.value = response.totalPages
+    auctions.value = response.responses
+  }
+})
+
+onBeforeMount(initData)
+</script>
 
 <template>
-  <h1>Auction</h1>
+  <div class="auction-container">
+    <div class="button-block">
+      <button class="createBtn" @click="openCreateModal">경매 등록</button>
+    </div>
+    <div class="table-block">
+      <table>
+        <thead>
+          <tr>
+            <th>경매 이름</th>
+            <th>입찰 시작가</th>
+            <th>최대 낙찰자</th>
+            <th>경매 시작 시각</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(auction, index) in auctions" :key="index">
+            <td>{{ auction.auctionName }}</td>
+            <td>{{ auction.startBidPrice.toLocaleString() }}원</td>
+            <td>{{ auction.maximumWinner }}</td>
+            <td>{{ formatDate(auction.startAt) }}</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <PaginationComponent
+      :on-change-page="onChangePage"
+      :request-page="requestPage"
+      :total-pages="totalPages"
+    />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@import url("@/assets/css/auction-manage.css");
+</style>
