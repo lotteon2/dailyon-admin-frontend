5a169988cc78823c33618fddc5b82f76e5650f0b
AMolRang <rlghks131@korea.ac.kr>
2024-01-16 11:18:34 +0900

    [Fix] 어드민 토큰 만료 해결

--- src/apis/utils/index.ts
+++ src/apis/utils/index.ts
@@ -2,6 +2,8 @@ import axios, { type AxiosInstance } from "axios"
 // vite에서 환경변수 값을 가져오기 위해서 import.meta 를 사용함
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
+let isAlertShown = false; 
+
 // 비로그인 axios instance
 const axiosApi = (baseURL: string) => {
   return axios.create({
@@ -19,8 +21,28 @@ const axiosAuthApi = (baseURL: string) => {
     config.headers.Authorization = `Bearer ${sessionStorage.getItem("accessToken")}`
     return config
   })
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+
+      if (error.response.status === 401) {
+        if (!isAlertShown) {
+          isAlertShown = true;
+
+          sessionStorage.clear();
+          alert("로그인 정보가 만료되었습니다.")
+          window.location.href = '/';
+        }
+      }
+    })
+
   return instance
 }
+
+
 
 export const defaultAxiosInstance: AxiosInstance = axiosApi(BASE_URL)
 export const authAxiosInstance: AxiosInstance = axiosAuthApi(BASE_URL)
